a6cc59fe55243cda31f4e302bef75b7b20389ac4
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-06 16:16:59 -0700

    Setup server

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+require('dotenv/config');
+
+const app = express();
+
+// Bring in routes
+const authRoute = require('./routes/auth-route');
+const apiRoute = require('./routes/api-route');
+
+// Body Parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Use routes
+app.use('/auth', authRoute);
+app.use('/api', apiRoute);
+
+// Serve static files in production mode
+if (process.env.PORT === 'production') {
+  app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+
+  // Home endpoint
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
+  });
+}
+
+// Global Error handler
+app.use((err, req, res, next) => {
+  // Set up default error
+  const defaultError = {
+    log: 'Error caught in global error handler',
+    status: 500,
+    msg: {
+      err: 'Check logs for more information',
+    },
+  };
+
+  // Update default error message with provided error if there is one
+  const output = Object.assign(defaultError, err);
+
+  res.send(output);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
