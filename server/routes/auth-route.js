40ae33f4fa9b2d1234851a3396b981b4ae1f9da2
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-06 16:16:38 -0700

    Add basic auth routes

--- server/routes/auth-route.js
+++ server/routes/auth-route.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
-//github login
-
+/**
+ * @route   GET /auth
+ * @desc    Allow users to sign into application using their Github account
+ * @access  Public
+ */
 router.get('/github', (req, res) => {
-    res.send('logging into github')
-})
+  res.send('logging into github');
+});
+
+/**
+ * @route   GET /auth
+ * @desc    Testing GET requests for auth route
+ * @access  Public
+ */
+router.get('/', (req, res) => {
+  res.send('Hitting auth GET endpoint!');
+});
+
+module.exports = router;
