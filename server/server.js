93736a5ae992e25272e13a288714acf689aabe3f
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-07 11:14:18 -0700

    Change paths for static files

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ app.use('/api', apiRoute);
 
 // Serve static files in production mode
 if (process.env.NODE_ENV === 'production') {
-  app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
+  app.use('/dist', express.static(path.resolve(__dirname, '../client/dist')));
 
   // Home endpoint
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../index.html'));
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
   });
 }
 
