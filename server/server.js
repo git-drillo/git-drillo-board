939c56881c94877fb6fcaa0045dbd70d7d7013dc
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-07 14:10:15 -0700

    Change port to 3000

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const path = require("path");
-const passportSetup = require("./config/passport-setup");
-const passport = require("passport");
+const express = require('express');
+const path = require('path');
+const passportSetup = require('./config/passport-setup');
+const passport = require('passport');
 
-require("dotenv/config");
+require('dotenv/config');
 
 const app = express();
 
 // Bring in routes
-const authRoute = require("./routes/auth-route");
-const apiRoute = require("./routes/api-route");
+const authRoute = require('./routes/auth-route');
+const apiRoute = require('./routes/api-route');
 
 // Body Parsing Middleware
 app.use(express.json());
@@ -17,33 +17,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 // Use routes
-app.use("/auth", authRoute);
-app.use("/api", apiRoute);
+app.use('/auth', authRoute);
+app.use('/api', apiRoute);
 
 // Serve static files in production mode
-if (process.env.NODE_ENV === "production") {
-  app.use("/dist", express.static(path.resolve(__dirname, "../client/dist")));
+if (process.env.NODE_ENV === 'production') {
+  app.use('/dist', express.static(path.resolve(__dirname, '../client/dist')));
 
   // Home endpoint
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/index.html"));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
   });
 }
-
-///TEST FOR DEV
-//app.get("/", (req, res) => {
-// res.status(200).sendFile(path.resolve(__dirname, "../client/index.html"));
-// res.send('HOME')
-//});
 
 // Global Error handler
 app.use((err, req, res, next) => {
   // Set up default error
   const defaultError = {
-    log: "Error caught in global error handler",
+    log: 'Error caught in global error handler',
     status: 500,
     msg: {
-      err: "Check logs for more information",
+      err: 'Check logs for more information',
     },
   };
 
@@ -53,6 +47,6 @@ app.use((err, req, res, next) => {
   res.send(output);
 });
 
-const PORT = process.env["PORT"] || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "::", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
