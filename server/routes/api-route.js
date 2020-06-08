ac907d9ad0ad72c8a86cd310e3cacd163d2f2d5d
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-07 21:27:28 -0700

    Add endpoint for getting tasks

--- server/routes/api-route.js
+++ server/routes/api-route.js
@@ -21,13 +21,13 @@ router.post('/', (req, res) => {
 });
 
 /**
- * @route   GET /api/projects/:id
+ * @route   GET /api/projects/:user_id
  * @desc    Returns an array of projects associated with a particular user
  * @access  Public (should be private)
  */
-router.get('/projects/:id', async (req, res, next) => {
+router.get('/projects/:user_id', async (req, res, next) => {
   try {
-    const id = req.params.id;
+    const id = req.params.user_id;
     const query = `
     SELECT * FROM projects
     WHERE project_owner = '${id}';`;
@@ -35,6 +35,36 @@ router.get('/projects/:id', async (req, res, next) => {
     const result = await db.query(query);
 
     return res.json({ data: result.rows });
+  } catch ({ message: msg }) {
+    return next({ msg });
+  }
+});
+
+/**
+ * @route   GET /api/tasks/:project_id
+ * @desc    Returns all tasks associated with a particular project in two arrays based on status
+ * @access  Public (should be private)
+ */
+router.get('/tasks/:project_id', async (req, res) => {
+  try {
+    const id = req.params.project_id;
+    const inProgressQuery = `
+      SELECT * FROM tasks
+      WHERE project_id = '${id}'
+      AND status = 'in progress';`;
+
+    const doneQuery = `
+      SELECT * FROM tasks
+      WHERE project_id = '${id}'
+      AND status = 'done';`;
+
+    const inProgressResult = await db.query(inProgressQuery);
+    const doneResult = await db.query(doneQuery);
+
+    res.json({
+      inProgress: inProgressResult.rows,
+      done: doneResult.rows,
+    });
   } catch ({ message: msg }) {
     return next({ msg });
   }
