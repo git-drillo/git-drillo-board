986e30dcb3bd84562c8c8a0d978d7ac696844266
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-07 21:24:49 -0700

    Add project action creators

--- client/actions/types.js
+++ client/actions/types.js
@@ -4,6 +4,10 @@ export const LOGOUT='LOGOUT';
 
 // User dashboard
 export const GET_PROJECTS='GET_PROJECTS';
+export const GET_PROJECT_TASKS = 'GET_PROJECT_TASKS';
+export const CREATE_TASK = 'CREATE_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const PROJECTS_LOADING = 'PROJECTS_LOADING';
 
 // Project Page
 // export const GET_PROJECTS='GET_PROJECTS';
