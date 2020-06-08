26dc536bc02b5a9852754baddf8898365f9efca4
Ronelle Caguioa <ronelle.caguioa@gmail.com>
2020-06-07 21:36:43 -0700

    Reorganize types

--- client/actions/types.js
+++ client/actions/types.js
@@ -2,12 +2,12 @@
 export const LOGIN='LOGIN';
 export const LOGOUT='LOGOUT';
 
-// User dashboard
+// Dashboard
 export const GET_PROJECTS='GET_PROJECTS';
+
+// Project dashboard
 export const GET_PROJECT_TASKS = 'GET_PROJECT_TASKS';
 export const CREATE_TASK = 'CREATE_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 export const PROJECTS_LOADING = 'PROJECTS_LOADING';
 
-// Project Page
-// export const GET_PROJECTS='GET_PROJECTS';
